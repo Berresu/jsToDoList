95e4ed22bdfd2964b5c98636aeb2523639f9b6ec
Berresu <90646787+Berresu@users.noreply.github.com>
2022-05-11 00:10:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+let girilen=document.getElementById("txtGirilen");
+let ekle=document.getElementById("btnEkle");
+let liste=document.getElementById("liste");
+let secilen;
+
+function listeyeEkle(){
+    let eklenecek=girilen.value;
+    let eklenen=document.createElement("li");
+    liste.appendChild(eklenen);
+    eklenen.innerHTML=eklenecek
+
+    let sil=document.createElement("div");
+    liste.appendChild(sil);
+    sil.innerHTML="X";
+    sil.setAttribute("class","close");
+    sil.addEventListener("click", listedenSil);
+
+    let yapıldı=document.createElement("div");
+    liste.appendChild(yapıldı);
+    yapıldı.innerHTML="✔";
+    yapıldı.setAttribute("class","bitti");
+    yapıldı.addEventListener("click", ()=>{
+        secilen=event.currentTarget.parentNode;
+        secilen.style.textDecoration="line-through";
+    });
+}
+
+function listedenSil(){
+    secilen=event.currentTarget.parentNode;
+    secilen.remove();
+}
